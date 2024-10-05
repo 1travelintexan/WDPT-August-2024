5b6256f570be70e17d7619f9deb38fb1d4d4b2da
Joshua <78570527+1travelintexan@users.noreply.github.com>
2024-10-05 16:08:01 +0200

    race car game

---
+++ module1/week6/day3/lab-dom-race-car/js/script.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  let ourGame;
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+  restartButton.addEventListener("click", () => {
+    window.location.reload();
+  });
+  //this is the event listener for the keyboard arrow keys
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowRight") {
+      ourGame.player.directionX = 2;
+    }
+    if (event.code === "ArrowLeft") {
+      ourGame.player.directionX = -2;
+    }
+
+    //this controls the player going up and down
+    if (event.code === "ArrowUp") {
+      ourGame.player.directionY = -2;
+    }
+    if (event.code === "ArrowDown") {
+      ourGame.player.directionY = 2;
+    }
+  });
+
+  //this event listener sets the directions to 0 when the keys are released
+  document.addEventListener("keyup", (event) => {
+    if (event.code === "ArrowRight") {
+      ourGame.player.directionX = 0;
+    }
+    if (event.code === "ArrowLeft") {
+      ourGame.player.directionX = 0;
+    }
+
+    //this controls the player going up and down
+    if (event.code === "ArrowUp") {
+      ourGame.player.directionY = 0;
+    }
+    if (event.code === "ArrowDown") {
+      ourGame.player.directionY = 0;
+    }
+  });
+  function startGame() {
+    console.log("start game");
+    // actually creates the game object and stores it in the variable
+    ourGame = new Game();
+    //this calls the start method on our Game class
+    ourGame.start();
+  }
+};
