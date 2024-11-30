f642f0d8073a85f93f0e72e7afbe8d0203387ea5
Joshua <horheyinc@gmail.com>
2024-11-30 11:26:02 +0100

    search bar

--- module2/week5/day2/CRUD/src/components/ProductListPage.jsx
+++ module2/week5/day2/CRUD/src/components/ProductListPage.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 export const ProductListPage = () => {
   const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [inputState, setInputState] = useState("");
   function getAllPosts() {
     axios
       .get("https://dummyjson.com/posts")
@@ -18,19 +20,41 @@ export const ProductListPage = () => {
     getAllPosts();
   }, []);
 
+  //functions
+  function handleSearch(e) {
+    e.preventDefault();
+    setSearchTerm(inputState);
+  }
   return (
     <div>
       <h1>Posts:</h1>
-      {posts.map((onePost) => {
-        return (
-          <div className="post-card" key={onePost.id}>
-            <Link to={`/posts/details/${onePost.id}`}>
-              <h3>Title: {onePost.title}</h3>
-            </Link>
-            <h3>Body: {onePost.body}</h3>
-          </div>
-        );
-      })}
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="search"
+          value={inputState}
+          onChange={(e) => setInputState(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+
+      {posts
+        //this is if you want on each character typed in the input
+        .filter((post) => {
+          if (post.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return true;
+          }
+        })
+        .map((onePost) => {
+          return (
+            <div className="post-card" key={onePost.id}>
+              <Link to={`/posts/details/${onePost.id}`}>
+                <h3>Title: {onePost.title}</h3>
+              </Link>
+              <h3>Body: {onePost.body}</h3>
+            </div>
+          );
+        })}
     </div>
   );
 };
