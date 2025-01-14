b231870c5b0599d0b04a458fee21e74e6d7c34d2
Joshua <horheyinc@gmail.com>
2025-01-14 21:39:39 +0100

    bcrypt and jwt

--- module3/week2/day3/CRUD/routes/user.routes.js
+++ module3/week2/day3/CRUD/routes/user.routes.js
@@ -1,5 +1,7 @@
 const UserModel = require("../models/User.model");
-
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { isAuthenticated } = require("../middlewares/auth.middleware");
 const router = require("express").Router();
 const newUser = {
   username: "Joshua",
@@ -8,14 +10,77 @@ const newUser = {
 };
 
 //route to create a new user
-router.post("/create-user", async (req, res) => {
+router.post("/signup", async (req, res) => {
+  //destructure
+  const { username, email } = req.body;
+  //check if the password is long enough
+
   try {
-    const createdUser = await UserModel.create(req.body);
-    console.log("user created, nice work", createdUser);
-    res.status(201).json({
-      createdUser: createdUser,
-      message: "nice work, user was created",
-    });
+    //first see if there is already a user with the email
+    const foundUser = await UserModel.findOne({ email });
+    // if the user exists... send error
+    if (foundUser) {
+      res.status(403).json({ message: "email already taken" });
+      //else continue creating the user.
+      //first encrypt the password
+      //then create the user without the original password
+    } else {
+      //create the salt
+      const theSalt = bcryptjs.genSaltSync(12);
+      //encrypt the password
+      const hashedPassword = bcryptjs.hashSync(req.body.password, theSalt);
+
+      //this is the long way, create a new variable and then store it in the DB
+      // const userToCreate = {
+      //   username: req.body.username,
+      //   email: req.body.email,
+      //   password: hashedPassword,
+      // };
+      const createdUser = await UserModel.create({
+        ...req.body,
+        password: hashedPassword,
+      });
+      res.status(200).json({ message: "signup successful", user: createdUser });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+//login route
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //first see if there is already a user with the email
+    const foundUser = await UserModel.findOne({ email }).lean();
+    // if the user exists... send error
+    if (!foundUser) {
+      res.status(403).json({ message: "Invalid Credentials" });
+    } else {
+      //make a variable that tells us if the password encrypted in the DB matches what they sent to the /login route
+      const doesItMatch = bcryptjs.compareSync(password, foundUser.password);
+      // console.log("does it match", doesItMatch);
+      //if the passwords do NOT match then we send back an error
+      if (!doesItMatch) {
+        res.status(403).json({ message: "Invalid Credentials" });
+        //else the passwords do match then we log in our user.
+      } else {
+        //remove the encrypted password from our foundUser variable
+        foundUser.password = "****";
+        //if the user logged in successfully, then we create a token with JWT
+        const authToken = jwt.sign(
+          { _id: foundUser._id, username: foundUser.username },
+          process.env.TOKEN_SECRET,
+          {
+            algorithm: "HS256",
+            expiresIn: "48h",
+          }
+        );
+        console.log("here is the authToken", authToken);
+        res.status(200).json({ message: "login successful", authToken });
+      }
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
@@ -76,7 +141,6 @@ router.patch("/update/:userId", async (req, res) => {
   try {
     const updatedUser = await UserModel.findByIdAndUpdate(theUserId, req.body, {
       new: true,
-      $inc: { __v: 1 },
     });
     console.log("here is the updated user", updatedUser);
     res.status(200).json({ updatedUser });
@@ -84,6 +148,12 @@ router.patch("/update/:userId", async (req, res) => {
     console.log(error);
     res.status(500).json(error);
   }
+});
+
+//verify the token route
+router.get("/verify", isAuthenticated, (req, res) => {
+  console.log("made it to the verify route", req.payload);
+  res.status(200).json({ currentUser: req.payload });
 });
 
 module.exports = router;
