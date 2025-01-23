de213b7d04e9963c8b5858d452a5c126bffd6386
Joshua <horheyinc@gmail.com>
2025-01-23 21:03:02 +0100

    DRUD complete

--- module3/our-fullstack-app/server/routes/todo.routes.js
+++ module3/our-fullstack-app/server/routes/todo.routes.js
@@ -35,11 +35,49 @@ router.get("/user-todos/:userId", async (req, res) => {
     //   const newTodo = { ...todo._doc, owner: todo.owner.username };
     //   return newTodo;
     // });
-    console.log("here are the users todos", todos, req.params.userId);
+    // console.log("here are the users todos", todos, req.params.userId);
     res.status(200).json(todos);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error finding the todos" });
+  }
+});
+//route to get details for one todo based on the id
+router.get("/one-todo/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+  try {
+    const oneTodo = await TodoModel.findById(todoId);
+    console.log("here is the one todo", oneTodo);
+    res.status(200).json(oneTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error finding the todo" });
+  }
+});
+//update one todo based on the id in the url
+router.put("/update/:todoId", async (req, res) => {
+  try {
+    const updatedTodo = await TodoModel.findByIdAndUpdate(
+      req.params.todoId,
+      req.body,
+      { new: true }
+    );
+    console.log("here is updated todo", updatedTodo);
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating the todo" });
+  }
+});
+//delete one todo based on the id in the url
+router.delete("/delete/:todoId", async (req, res) => {
+  try {
+    const deletedTodo = await TodoModel.findByIdAndDelete(req.params.todoId);
+    console.log("deleted todo", deletedTodo);
+    res.status(200).json(deletedTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error deleting the todo" });
   }
 });
 module.exports = router;
